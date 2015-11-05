c4eebe5fd8fa6b22952bbb91a51fc2588211da9a
maxidalesio <maxi.dalesio@gmail.com>
2015-11-05 17:00:34 -0300

    Se agrega tipo al script de la BD

--- bd/tpcarniceria.sql
+++ bd/tpcarniceria.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-11-2015 a las 20:30:53
+-- Tiempo de generación: 05-11-2015 a las 20:59:48
 -- Versión del servidor: 5.6.24
 -- Versión de PHP: 5.6.8
 
@@ -33,6 +33,7 @@ CREATE TABLE IF NOT EXISTS `productos` (
   `descripcion` varchar(30) NOT NULL,
   `info` varchar(50) DEFAULT NULL,
   `precio` float NOT NULL,
+  `tipo` varchar(30) NOT NULL,
   `foto` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -53,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `provincia` varchar(20) NOT NULL,
   `telefono` varchar(20) NOT NULL,
   `tipo` varchar(10) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -61,7 +62,8 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 
 INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `mail`, `clave`, `direccion`, `localidad`, `provincia`, `telefono`, `tipo`) VALUES
 (9, 'Maxi', 'Dalesio', 'maxi.dalesio@gmail.com', 'e3ae69b7485a1ba5dfec809789d8a55b', 'Comodoro Rivadavia 3729', 'Sarandi', 'Buenos Aires', '01143531675', 'admin'),
-(25, 'Pepe', 'Pepino', 'pepe@lalala.com', '28f2b95533afb47cbec1d823b0f1a941', 'Avenida Mitre 810', 'Avellaneda', 'Buenos Aires', '43531222', 'user');
+(25, 'Pepe', 'Pepino', 'pepe@lalala.com', '28f2b95533afb47cbec1d823b0f1a941', 'Avenida Mitre 810', 'Avellaneda', 'Buenos Aires', '43531222', 'user'),
+(26, 'Alf', 'Alfito', 'alf@alf.com', '893f53c159eab9178ab181bad8da4262', 'Cassazza 332', 'Villa Dominico', 'Buenos Aires', '43321122', 'user');
 
 --
 -- Índices para tablas volcadas
@@ -87,7 +89,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=26;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=27;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
