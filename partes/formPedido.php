19852612b531ca0fc6a26841fcc3c6e391ffd5db
maxidalesio <maxi.dalesio@gmail.com>
2015-12-09 20:18:26 -0300

    Se agregan detalles de carrito, considerar ponerlo en tabla

--- partes/formPedido.php
+++ partes/formPedido.php
@@ -11,10 +11,6 @@ $(document).ready(
 		});
 	});
 </script>
-<?php
-echo "acá tabla con descripción y cantidad de cada producto seleccionado";
-echo "</br>";
-?>
 
 
 <form class="form-horizontal" id="FormProducto" method="post" onsubmit="GuardarProducto();return false" enctype="multipart/form-data">
@@ -22,12 +18,32 @@ echo "</br>";
 	<fieldset>
 		<legend>Confirmar Pedido</legend>
 
+		<div align="right">
+				<h4>
+					<?php
+					session_start();
+					$cartTotal= 0;
+					foreach ($_SESSION["cart_array"] as $each_item) { 
+						$item_id = $each_item['item_id'];
+						$item_quantity= $each_item['quantity'];
+						$prod = producto::TraerUnProducto($item_id);
+						$prodTotal = $item_quantity * $prod->precio;
+						$cartTotal = $cartTotal + $prodTotal;
+						echo "$prod->descripcion - Cantidad: $item_quantity - Precio Unitario: $".$prod->precio;
+						echo"</br>";
+					}
+					echo "</br>";
+					echo"Total: $".$cartTotal;
+					echo "</br>";
+					?>
+				</h4>
+		</div>
 		<!-- Text input-->
 		<div class="form-group">
 			<label class="col-md-4 control-label" for="datepicker">Fecha:</label>  
 			<div class="col-md-4">
 				<input id="datepicker" name="datepicker" type="text"class="form-control" required="">
-				<span class="help-block">Ingrese fecha para el Pedido, recuerde que es con mínimo 5 días de anticipación. Las fechas blancas no podrán ser elegidas.</span>  
+				<span class="help-block">Ingrese fecha para el Pedido, recuerde que es con mínimo 5 días de anticipación.</span>  
 			</div>
 		</div>
 		<!-- Multiple Radios (inline) -->
@@ -46,7 +62,7 @@ echo "</br>";
 		<!-- Buttons-->
 	</div><div class="btn-group pull-right">
 	<button id="btnAtras" onclick="Mostrar('Productos'); return false" class="btn btn-sucess">Continuar Comprando</button>
-	<input type="submit" id="btnAceptar" name="btnAceptar" class="btn btn-danger" value="Aceptar">
+	<input type="submit" id="btnAceptar" name="btnAceptar" class="btn btn-danger" value="Confirmar">
 </div>
 </fieldset>    
 </form>
